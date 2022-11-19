f2637f21cadeb96efec3a9b67b9146592c4e5e7a
vandalfar <micande1@uat.edu>
2022-11-19 00:57:38 -0600

    functional
    Created functions

---
+++ class-version.js
@@ -0,0 +1,48 @@
+function countDown() {
+    var currTime = 3;
+    //make countdown visible
+        document.getElementById("countdownTimer").style.display = "block";
+    //timer for seconds left
+    setTimeout(function () {
+        //code goes here for first timer
+        document.getElementById("countdownTimer").innerHTML = currTime;
+        currTime = currTime - 1;
+    }, 1000);
+
+
+    //timer for seconds left
+    setTimeout(function () {
+        //code goes here for first timer
+        document.getElementById("countdownTimer").innerHTML = currTime;
+        currTime = currTime - 1;
+    }, 2000);
+
+
+    //timer for seconds left
+    setTimeout(function () {
+        //code goes here for first timer
+        document.getElementById("countdownTimer").innerHTML = currTime;
+        currTime = currTime - 1;
+    }, 3000);
+
+
+    //timer for seconds left
+    setTimeout(function () {
+        //code goes here for first timer
+        document.getElementById("countdownTimer").innerHTML = currTime;
+        currTime = currTime - 1;
+    }, 4000);
+
+    //timer for seconds left
+    setTimeout(function () {
+        //code goes here for first timer
+        document.getElementById("countdownTimer").innerHTML = "GO! ";
+        currTime = currTime - 1;
+        alert(" And the race has begun");
+    }, 5000);
+
+}
+ 
+   
+
+ 
