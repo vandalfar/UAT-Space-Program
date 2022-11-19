d5cea1a5453d16ab8dff1934ef7dff123dda722e
vandalfar <micande1@uat.edu>
2022-11-19 16:10:33 -0600

    beautified
    changed button position and colors to beautify the page. Aligned second paragraph with id #shoutout to center. Centered the buttons with <center> tag wrapped around both of them.

--- UATSpaceProgram.js
+++ UATSpaceProgram.js
@@ -6,65 +6,72 @@ function countDown()  //for an unknown reason, the function takes two seconds to
 { 
     //timeout code from class-version.js
     setTimeout(function () {
-        //first
+        //first (10)
         document.getElementById("countdownTimer").innerHTML = currTime;
         currTime = currTime - 1;
         //using the millisecond as an exact value seems to move the stage of the function to the assigned slot
     }, 0000);
     //timeout code from class-version.js
     setTimeout(function () {
-        //second
+        //second (9)
         document.getElementById("countdownTimer").innerHTML = currTime;
         currTime = currTime - 1;
         //making this appear after lower numbers on the timer
     }, 1000);
     //timeout code from class-version.js
     setTimeout(function () {
-        //third
+        //third (8)
         document.getElementById("countdownTimer").innerHTML = currTime;
         currTime = currTime - 1;
         //during development, I accidentally mixed around a few of these timings
     }, 2000);
     //timeout code from class-version.js
     setTimeout(function () {
-        //fourth
+        //fourth (7)
         document.getElementById("countdownTimer").innerHTML = currTime;
         currTime = currTime - 1;
         //and this block appeared before the previous block
+        //now this number displays for two seconds?
+        //split milliseconds?
     }, 3000);
     //timeout code from class-version.js
     setTimeout(function () {
-        //fifth
+        //fifth (6)
         document.getElementById("countdownTimer").innerHTML = currTime;
         currTime = currTime - 1;
         //this block appeared normally
     }, 4000);
     //timeout code from class-version.js
     setTimeout(function () {
+        //sixth (5)
         //this made me realize that the millisecond counter when using setTimeout occurs numerically
         document.getElementById("countdownTimer").innerHTML = currTime;
         currTime = currTime - 1;
     }, 5000);
     //timeout code from class-version.js
     setTimeout(function () {
+        //seventh (4)
         //and not in a cascading order
         document.getElementById("countdownTimer").innerHTML = currTime;
         currTime = currTime - 1;
     }, 6000);
     //timeout code from class-version.js
     setTimeout(function () {
+        //eigth (3)
         //or even one after the other
         document.getElementById("countdownTimer").innerHTML = currTime;
         currTime = currTime - 1;
     }, 7000);
     //timeout code from class-version.js
     setTimeout(function () {
+        //ninth (2)
         //W3C schools has a notice that setTimeout() only runs once, and repeated occurances should use setInterval()
         document.getElementById("countdownTimer").innerHTML = currTime;
         currTime = currTime - 1;
     }, 8000);
     //timeout code from class-version.js
     setTimeout(function () {
+        //tenth (1)
         //leading to this behavior with the millisecond count
         //each occurance of setTimeout() is run together, with the actions happening in order of the milliseconds defined within the code
         document.getElementById("countdownTimer").innerHTML = currTime;
@@ -72,10 +79,16 @@ function countDown()  //for an unknown reason, the function takes two seconds to
     }, 9000);
     //timeout code from class-version.js
     setTimeout(function () {
+        //eleventh (0)
         //display Blastoff message
         document.getElementById("countdownTimer").innerHTML = "Blastoff!";
         }, 10000);
-
+    //timeout code from class-version.js
+    setTimeout(function () {
+        //reset currTime variable to prevent negative numbers from appearing on reclick
+        //set at the same time as previous action gets rid of (7) displaying for two seconds
+        currTime = 10;
+        }, 10000)
 }
 function stopCountDown()
 //stopCounDown does not stop the timer - interrupts can happen with a loop...
--- unhide.js
+++ unhide.js
@@ -1,4 +1,4 @@
 function unhide(){
-        //make hidden content visible
+        //make hidden content visible. Created a second .js file as not to edit class-version.js as instructed by the teacher.
         document.getElementById("countdownTimer").style.display = "block";
 }
