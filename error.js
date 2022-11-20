18d0cded67aa3d8064c95070849e01f1c6179ed5
vandalfar <micande1@uat.edu>
2022-11-20 00:03:33 -0600

    final
    updated CSS, JavaScript, and HTML to be more human readable and added/updated comments

--- error.js
+++ error.js
@@ -1,15 +1,10 @@
 //created by Michael Anderson
-/*function unhide(){
-        //make countdown visible
-        document.getElementById("countdownTimer").style.display = "block";
-}*/
-function stopCountDown()
-//stopCounDown does not stop the timer
-{
-    /*Creates a fake error that targets #countdownParagraph and #quoteBlock via id and updates the text inside to  what's quoted below,
-      could use a loop to check if there were still 10 seconds on the clock, making a different line of text appear.*/
-    document.getElementById("countdownParagraph").innerHTML = "> Error: Ignition detected!";
-    //alert shows whenever the "Stop" button is pressed, which also causes the above paragraph to contain text
-    alert("There was an error.\n> Error: Ignition detected!\nThe rocket still launched!");
-document.getElementById("quoteBlock").innerHTML = "<br><q>Each time you fail, you have eliminated another wrong option.</q><br>   - Thomas A. Edison";
+/*stopCountDown does not stop the timer, and instead gives a fake error and updates the paragraphs selected by id*/
+function stopCountDown() {
+  /*Creates a fake error that targets #countdownParagraph and #quoteBlock via id and updates the text inside*/
+  document.getElementById("countdownParagraph").innerHTML = "> Error: Ignition detected!";
+  //alert shows whenever the "Stop" button is pressed, which also causes the above paragraph to contain text
+  //added \n to create new line elements
+  alert("There was an error.\n \n> Error: Ignition detected!\n \nThe rocket still launched!");
+  document.getElementById("quoteBlock").innerHTML = "<br><q>Each time you fail, you have eliminated another wrong option.</q><br>   - Thomas A. Edison";
 }
